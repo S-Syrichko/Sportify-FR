b7b652a0addf68299d64fc08250e5e47fbefd727
S-Syrichko <106162070+S-Syrichko@users.noreply.github.com>
2023-12-11 18:41:38 +0100

    deploy: Migrated backend data to mockApi and enabled mocking

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-//import axios from "./mocks/mockApi";
+//import axios from "axios";
+import axios from "./mocks/mockApi";
 
 import { User, UserActivity, UserSessions, UserPerformance } from "./DataTypes";
 
